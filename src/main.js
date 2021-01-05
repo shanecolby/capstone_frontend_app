a05b031a0e0aed2dd62a1833b58fce0d4feee7fe
shane <shanecolbyzero8@gmail.com>
2021-01-04 20:17:11 -0600

    home page index with working name,focus and image url

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 import axios from "axios";
 
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="exercise in exercises">
+      <h1>{{ exercise.name }}</h1>
+      <h2>{{ exercise.focus }}</h2>
+      <h3>{{ exercise.image_url }}</h3>
+    </div>
+    
   </div>
 </template>
 
@@ -8,13 +14,25 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome to WorkoutCaddy!",
+      exercises: [
+        { id: 1, name: "workout1", image_url: "www.google.com" },
+        { id: 2, name: "workout2", image_url: "www.g.com" },
+        { id: 3, name: "workout3", image_url: "www.goo3ee.com" },
+      ],
     };
   },
-  created: function () {},
+  created: function () {
+    axios.get("/api/exercises").then((response) => {
+      console.log("exercise index...");
+      this.exercises = response.data;
+    });
+  },
   methods: {},
 };
 </script>
