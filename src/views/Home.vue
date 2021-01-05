267c16d220e9be6edc4a6a2ced8db0c505b7b395
shane <shanecolbyzero8@gmail.com>
2021-01-05 07:05:42 -0600

    add create a exercise button on main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,15 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <hr>
+    <h2>Create an exercise</h2>
+    <button v-on:click="createExercise()">Create exercise</button>
     <!-- <button v-on:click="exercisesIndex()">Show Exercises</button> -->
   
     <div v-for="exercise in exercises">
    
       <h1>{{ exercise.name }}</h1>
       <h2>{{ exercise.focus }}</h2>
-      <p><img v-bind:src="exercise.image_url"></p>
+      <p><img v-bind:src="exercise.image_url" v-bind:alt="exercise.name"></p>
       <button v-on:click="exercisesShow()">Show Exercise</button>
       <br>
       <button v-on:click="exercisesIndex()">Add Exercise to Workout</button>
@@ -44,6 +46,17 @@ export default {
       axios.get("/api/exercises").then((response) => {
         console.log(response.data);
         this.exercises = response.data;
+      });
+    },
+    createExercise: function () {
+      console.log("creating exercise...");
+      var params = {
+        name: "name",
+        focus: "all",
+        image_url: "google",
+      };
+      axios.post("/api/exercises", params).then((response) => {
+        console.log(response.data);
       });
     },
   },
