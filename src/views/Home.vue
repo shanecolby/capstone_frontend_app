12bfcb415431e9a4b0a168735273cf511dac0432
shane <shanecolbyzero8@gmail.com>
2021-01-04 20:42:08 -0600

    index for all exercises on main page without clicking button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <button v-on:click="exercisesIndex()">Show Exercises</button>
     <div v-for="exercise in exercises">
       <h1>{{ exercise.name }}</h1>
       <h2>{{ exercise.focus }}</h2>
       <h3>{{ exercise.image_url }}</h3>
+      <hr>
     </div>
     
   </div>
@@ -20,11 +22,7 @@ export default {
   data: function () {
     return {
       message: "Welcome to WorkoutCaddy!",
-      exercises: [
-        { id: 1, name: "workout1", image_url: "www.google.com" },
-        { id: 2, name: "workout2", image_url: "www.g.com" },
-        { id: 3, name: "workout3", image_url: "www.goo3ee.com" },
-      ],
+      exercises: [],
     };
   },
   created: function () {
@@ -33,6 +31,14 @@ export default {
       this.exercises = response.data;
     });
   },
-  methods: {},
+  methods: {
+    exercisesIndex: function () {
+      console.log("exercises index...");
+      axios.get("/api/exercises").then((response) => {
+        console.log(response.data);
+        this.exercises = response.data;
+      });
+    },
+  },
 };
 </script>
