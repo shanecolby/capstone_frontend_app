9c69d24ef8939539661472ab097f65ff6e014efa
shane <shanecolbyzero8@gmail.com>
2021-01-07 21:04:56 -0600

    create workout page to display selected exercises,not complete

--- src/views/CreatedWorkout.vue
+++ src/views/CreatedWorkout.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="CreatedWorkout">
     <h1>{{ message }}</h1>
-    <!-- <div v-for="selected_exercise in selected_exercises">
-    </div> -->
+    <!-- <div v-for="selected_exercise in selected_exercises"> -->
+      <h1>{{selected_exercises}}</h1>
+      <!-- <h1>{{selected_exercises.name}}</h1>
+      <h1>{{selected_exercises.focus}}</h1>
+      <h1>{{selected_exercises.image_url}}</h1> -->
+      
+    </div>
   </div>
 </template>
 
@@ -15,7 +20,11 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Created Workout!",
+      message: "Created Workout",
+      selected_exercises: [],
+      name: "",
+      focus: "",
+      image_url: "",
     };
   },
   created: function () {
@@ -24,6 +33,14 @@ export default {
       this.selected_exercises = response.data;
     });
   },
-  methods: {},
+  methods: {
+    createWorkout: function () {
+      console.log("user workout...");
+      axios.get("/api/selected_exercises").then((response) => {
+        console.log(response.data);
+        this.selected_exercises = response.data;
+      });
+    },
+  },
 };
 </script>
